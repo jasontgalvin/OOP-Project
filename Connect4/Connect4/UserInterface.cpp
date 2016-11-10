ef7beb9aacb39fc121a5525b8f96bb95010b2d25
jason <jason@JASONS-LAPTOP>
2016-11-10 15:33:58 -0500

    Added more functionality to UserInterface

--- Connect4/Connect4/UserInterface.cpp
+++ Connect4/Connect4/UserInterface.cpp
@@ -1,6 +1,10 @@
 #include "UserInterface.h"
 #include <iostream>
 #include <stdlib.h>
+#include "Player.h"
+#include "Board.h"
+#include "Game.h"
+#include "Computer.h"
 using namespace std;
 
 
@@ -21,16 +25,27 @@ void UserInterface::displayStartup()
 	cout << "Please select an option " << endl;
 	cout << "1. Start Game\n2. View Scores\n3. Quit" << endl;	//Ive included a View Scores option, this can easily be removed if we dont have the time
 
+	Player *player = new Player();
+
 	while ((input != "1") && (input != "2") && (input != "3"))
 	{
 		cin >> input;
 		if (input == "1")
 		{
 			//START GAME
+			Game *game = new Game();
+			Board *board = new Board();
+			Computer *cpu = new Computer();
+
+			cout << "Input Name: " << endl;
+			cin >> input;
+			player->setName(input);
 		}
 		else if (input == "2")
 		{
 			//VIEW SCORES
+			cout << player->getName() << "'s score is: " << player->getScore() << endl;
+
 		}
 		else if (input == "3")
 		{
