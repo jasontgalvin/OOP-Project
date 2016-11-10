7d55dad7739b5da3b0dcc815b4a390cff42ede29
jason <jason@JASONS-LAPTOP>
2016-11-10 13:04:09 -0500

    UserInterface:
    Create initial startup display for user to select options

--- Connect4/Connect4/UserInterface.cpp
+++ Connect4/Connect4/UserInterface.cpp
@@ -1,5 +1,6 @@
 #include "UserInterface.h"
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 
@@ -13,10 +14,36 @@ UserInterface::~UserInterface()
 {
 }
 
-void UserInterface::displayStartup() {
+void UserInterface::displayStartup() 
+{
+	string input = "";
+	cout << "Welcome to Connect 4 " << endl;
+	cout << "Please select an option " << endl;
+	cout << "1. Start Game\n2. View Scores\n3. Quit" << endl;	//Ive included a View Scores option, this can easily be removed if we dont have the time
+
+	while ((input != "1") && (input != "2") && (input != "3"))
+	{
+		cin >> input;
+		if (input == "1")
+		{
+			//START GAME
+		}
+		else if (input == "2")
+		{
+			//VIEW SCORES
+		}
+		else if (input == "3")
+		{
+			exit(0);	//Program terminates
+		}
+		else {
+			cout << "Error, invalid input" << endl;
+		}
+	}
 
 }
-void UserInterface::displayWinner(Disc disc) {
+void UserInterface::displayWinner(Disc disc) 
+{
 
 }
 void UserInterface::displayInvalidMove() {}
